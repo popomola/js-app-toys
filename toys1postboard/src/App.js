1f9454eba13032c7aa4f22a5a2912e1329f3bd64
popomola <mrsimple1015@naver.com>
2021-08-19 17:40:30 +0900

    Update App.js

--- toys1postboard/src/App.js
+++ toys1postboard/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PostBoard from './Components/PostBoard.js';
 
 function App() {
   return (
     <div className="App">
-    
+      <PostBoard />
     </div>
   );
 }
