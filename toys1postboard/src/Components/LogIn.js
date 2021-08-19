b67559ceacf666605b34f92fd64a4f472ac009f3
popomola <mrsimple1015@naver.com>
2021-08-19 18:50:38 +0900

    Create LogIn.js

---
+++ toys1postboard/src/Components/LogIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LogIn = ()=>{
+    return(
+        <div>
+            <form>
+                <input placeholder="아이디"></input>
+                <input placeholder="비밀번호"></input>
+            </form>
+        </div>
+    )
+}
+
+export default LogIn
