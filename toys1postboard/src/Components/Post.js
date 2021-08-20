f6bd478a4069259f5a0a9f412faee124ac940a26
popomola <mrsimple1015@naver.com>
2021-08-20 15:09:41 +0900

    Update Post.js

--- toys1postboard/src/Components/Post.js
+++ toys1postboard/src/Components/Post.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import Modal from 'react-modal';
+import {useState} from 'react';
 
 const Post = ({writerInfo, body})=>{
+    const [modalIsOpen, setIsOpen] = useState(true);
     return(
-        <div>
-            <div>
-                {writerInfo}
-            </div>
-            <div>
-                {body}
-            </div>
-        </div>
+        <>
+        {/* 따로 해보니까 모달 자체는 잘 나온다. Route에 문제가 있어서 얘가 화면에 안 뜨는 것. */}
+            <Modal isOpen={modalIsOpen}>
+            <p>{writerInfo}</p>
+            <p>{body}</p>
+            <button onClick={()=>{setIsOpen(false)}}>닫기</button>
+            </Modal>
+        </>
     )
 }
   
