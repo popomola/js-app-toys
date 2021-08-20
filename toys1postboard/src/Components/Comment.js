1cfdbbf12ff8b34327b61041e5917bcdbc2465af
popomola <mrsimple1015@naver.com>
2021-08-20 15:47:18 +0900

    Create Comment.js

---
+++ toys1postboard/src/Components/Comment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Comment = ()=>{
+
+    return(
+        <>
+            <p>댓글창</p>
+            <div>
+                완전 대박!
+            </div>
+        </>
+    )
+}
+  
+export default Comment;
