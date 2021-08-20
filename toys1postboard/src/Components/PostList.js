d0364a58d750ec24df0c7cd7b9ad9ccb3452d40a
popomola <mrsimple1015@naver.com>
2021-08-20 15:43:23 +0900

    Update PostList.js

--- toys1postboard/src/Components/PostList.js
+++ toys1postboard/src/Components/PostList.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {BrowserRouter as Router,Switch,Route, Link} from "react-router-dom";
 
 import PostItem from './PostItem';
-import PostBody from './PostBody';
-import PostBoard from './PostBoard';
 
 //각각의 PostItem를 <a>로 감싸서 링크를 달아놓고,
 //해당 링크가 해당 게시글의 본문과 이어지도록? // 다른 방법은 아직 잘..
@@ -14,33 +12,25 @@ const PostList = ({postListTitle, userDB})=>{
             <div className="postList">
                 <p>{postListTitle}</p>
                 
-                <Link to={`/postbody${userDB[0].writer}`}>
-                <PostItem 
-                writer={userDB[0].writer} 
-                date={userDB[0].date} 
-                title={userDB[0].title} 
-                body={userDB[0].body} />
+                {/* 계속 이 Link관련해서 골머리를 앓았다. 암만 Link를 눌러도 빈 페이지만 나와서.
+                그런데 Component폴더 밖의 App파일에서 PostBoard를 이미 Route에서 쓰는데
+                또 PostBoard 내부의 이 PostList컴포넌트가  Route를 쓰니까 path가 꼬인 모양이었다.
+                */}
+                <Link to='/posting'>
+                    <PostItem 
+                    writer={userDB[0].writer} 
+                    date={userDB[0].date} 
+                    title={userDB[0].title} 
+                    body={userDB[0].body} />
                 </Link>
                 
-                <Link to={`/postbody${userDB[1].writer}`}>
-                <PostItem 
-                writer={userDB[1].writer} 
-                date={userDB[1].date} 
-                title={userDB[1].title} 
-                body={userDB[1].body} />
+                <Link to='/posting2'>
+                    <PostItem 
+                    writer={userDB[1].writer} 
+                    date={userDB[1].date} 
+                    title={userDB[1].title} 
+                    body={userDB[1].body} />
                 </Link>
-
-                <Switch>
-
-                    <Route path={`/postbody${userDB[0].writer}`}>
-                        <PostBoard writerInfo={userDB[0].writer}/>
-                    </Route>
-
-                    <Route path={`/postbody${userDB[1].writer}`}>
-                        <PostBoard writerInfo={userDB[1].writer}/>
-                    </Route>
-
-                </Switch>
             </div>
         </>
     )
