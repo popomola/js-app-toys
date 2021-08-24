3d6ae354efddcbae9b1982af5e956503053970f5
popomola <mrsimple1015@naver.com>
2021-08-24 16:16:12 +0900

    Update App.js

--- toys2postboard/src/App.js
+++ toys2postboard/src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Button }  from '@material-ui/core';
+import Modal from 'react-modal';
+
+import PostBody from './PostBody';
+
+const textDB = [
+
+  '고민이 생겼어요. 저는 사실 바보입니다...',
+  '고민이 생겼어요. 저는 사실 주식을 사랑합니다...',
+]
 
 function App() {
+  const [IsOpen, setIsOpen] = useState(false)
+
   return (
-    <div >
-     hello
+    <div>
+      
+      <Modal isOpen={IsOpen} ariaHideApp={false}>
+        <Button onClick={() => {setIsOpen(false)}} color='secondary'>
+          창닫기
+        </Button>   
+      </Modal>
+
+      <>
+        <Button onClick={() => {setIsOpen(true)}} color='primary'>
+          <PostBody maintext={textDB[0]}/>
+        </Button>
+        <Button onClick={() => {setIsOpen(true)}} color='primary'>
+          <PostBody maintext={textDB[1]}/>
+        </Button>
+      </>
+
     </div>
   );
 }
