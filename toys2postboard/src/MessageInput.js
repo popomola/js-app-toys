ade9ccd98b44a2076acd887ba282837d6c8bd52e
popomola <mrsimple1015@naver.com>
2021-08-27 14:57:35 +0900

    Create MessageInput.js

---
+++ toys2postboard/src/MessageInput.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function MessageInput() {
+    return(
+      <>
+       {/* 
+            메시지 입력창, 입력창의 내용을 받아 화면에 임시 표시
+       */}
+       <div>
+           <div>
+               <textarea>
+                    
+               </textarea>
+           </div>
+       </div>
+      </>
+    );
+  }
+  
+  export default MessageInput;
